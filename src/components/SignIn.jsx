9307c894b65f4aec7e4755b256faa07cfd88af0f
Zion98 <adeyemioluwasegun98@gmail.com>
2021-07-31 15:38:49 +0100

    er

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useContext } from "react";
-import { Link, } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Context } from "../context/Context";
 import { Form } from "./styled/index";
 const Signin = () => {
   const userRef = useRef();
   const passwordRef = useRef();
-  
-
-  const { dispatch,state } = useContext(Context);
+console.log("rsgethry")
+  const history = useHistory();
+  console.log(history)
+  const { dispatch} = useContext(Context);
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -29,9 +30,10 @@ const Signin = () => {
       .then((data) => {
         console.log("datacvb", data.data);
         dispatch({ type: "LOGIN_SUCCESS", payload: data.data });
-		localStorage.setItem('user', JSON.stringify(data.data));
-		window.location.href = '/articles';
-	
+        localStorage.setItem("user", JSON.stringify(data.data));
+        // window.location.href = '/articles';
+        history.push("/articles");
+		console.log(history)
       })
       .catch((err) => {
         dispatch({ type: "LOGIN_ERROR" });
@@ -39,38 +41,41 @@ const Signin = () => {
       });
   };
 
-
-  console.log("Afre",state.user)
+//   console.log("Afre", state.user);
 
   return (
     <>
       <Form>
-		  <div className="containerx">
-        <p className="logo">The Info</p>
-        <h2 className="sign-in-title">Sign in</h2>
-        <p className="proceed">Please enter your credentials to proceed</p>
-        <form onSubmit={handleSubmit} className="forms">
-          <label className="address mail">EMAIL ADDRESS</label>
-          <input type="email" name="email" autoComplete={true} ref={userRef} />
+        <div className="containerx">
+          <p className="logo">The Info</p>
+          <h2 className="sign-in-title">Sign in</h2>
+          <p className="proceed">Please enter your credentials to proceed</p>
+          <form onSubmit={handleSubmit} className="forms">
+            <label className="address mail">EMAIL ADDRESS</label>
+            <input
+              type="email"
+              name="email"
+              autoComplete={true}
+              ref={userRef}
+            />
 
-          <div className="password-title">
-            <label className="password">PASSWORD</label>
-            <p className="forgot">Forgot password?</p>
-          </div>
-          <input type="password" name="password" ref={passwordRef} />
+            <div className="password-title">
+              <label className="password">PASSWORD</label>
+              <p className="forgot">Forgot password?</p>
+            </div>
+            <input type="password" name="password" ref={passwordRef} />
 
-          <button type="submit">Sign in</button>
+            <button type="submit">Sign in</button>
 
-          <p className="have-account">
-            Don't have an account?{" "}
-            <Link to="/register">
-              <span>Sign up</span>
-            </Link>
-          </p>
-        </form>
-		</div>
+            <p className="have-account">
+              Don't have an account?{" "}
+              <Link to="/register">
+                <span>Sign up</span>
+              </Link>
+            </p>
+          </form>
+        </div>
       </Form>
-     
     </>
   );
 };
